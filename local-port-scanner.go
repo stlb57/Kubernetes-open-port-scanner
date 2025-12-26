b74fdc0b3d36c94dba3c8ba77c026cfa6c603b12
lb57 <saarthaktyagi1@gmail.com>
2025-12-26 21:43:47 +0530

    Added channels and goroutines for concurrency

--- local-port-scanner.go
+++ local-port-scanner.go
@@ -4,23 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+
 	// "os"
-	// "time"
+	"time"
 )
 
-func main() {
-	for port := 1; port <= 20; port++ {
-		address := fmt.Sprintf("localhost:%d", port)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			fmt.Println("Port closed")
-		} else {
-			fmt.Println("Port open")
+func port_checker(port int, c chan string) {
+	address := fmt.Sprintf("localhost:%d", port)
+	conn, err := net.DialTimeout("tcp", address, 1*time.Second)
+	if err == nil {
+		if port == 80 {
 			fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 			status, err := bufio.NewReader(conn).ReadString('\n')
 			fmt.Println(status, err)
-			defer conn.Close()
 		}
+		c <- "Port open"
+		conn.Close()
+	}
+}
+func main() {
+	c := make(chan string)
+	for port := 1; port <= 1024; port++ {
+		go port_checker(port, c)
+	}
+	for port := 1; port <= 1024; port++ {
+		fmt.Println(<-c)
 	}
 
 }
